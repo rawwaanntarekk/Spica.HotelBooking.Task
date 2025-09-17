d09b063ebfa2d4ca48d63c34740a869cc4963b3c
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-09-17 17:14:12 +0300

    Add(Infrastructure): Add DbContext and main tables.

---
+++ Booking.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using DbBooking = Booking.Domain.Entities.Booking;
+
+
+namespace Booking.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<DbBooking> Bookings { get; set; }
+
+
+    }
+    
+}
