114d532700e5567f6cf04c1ce823cb41c24c4470
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-09-17 18:25:52 +0300

    Add(Data Access): Add Repository implementation.

---
+++ Booking.Domain/Interfaces/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using DbBooking = Booking.Domain.Entities.Booking;
+
+namespace Booking.Domain.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task<IEnumerable<DbBooking>> GetAllBookingsAsync(bool withTracking = false);
+        Task<DbBooking?> GetBookingByIdAsync(int id);
+        Task AddBookingAsync(DbBooking booking);
+        void UpdateBooking(DbBooking booking);
+        void DeleteBooking(DbBooking booking);
+    }
+}
---
+++ Booking.Infrastructure/Data/Repositories/BookingRepository.cs
@@ -0,0 +1,33 @@
+﻿using Booking.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using DbBooking = Booking.Domain.Entities.Booking;
+
+namespace Booking.Infrastructure.Data.Repositories
+{
+    public class BookingRepository(AppDbContext context) : IBookingRepository
+    {
+        public async Task<IEnumerable<DbBooking>> GetAllBookingsAsync(bool withTracking = false)
+        {
+            return withTracking ?
+                   await context.Bookings.ToListAsync()
+                 : await context.Bookings.AsNoTracking().ToListAsync();
+        }
+
+
+        public async Task<DbBooking?> GetBookingByIdAsync(int id)
+        => await context.Bookings.FindAsync(id);
+
+        public async Task AddBookingAsync(DbBooking booking)
+        => await context.Bookings.AddAsync(booking);
+
+        public void UpdateBooking(DbBooking booking)
+        =>  context.Bookings.Update(booking);
+
+        public void DeleteBooking(DbBooking booking)
+        => context.Bookings.Remove(booking);
+
+
+
+
+    }
+}
